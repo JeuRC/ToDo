590a4ab14f9e3296ad1f289448ea8bb8badf9d51
JeuRC <jeurojascastillo@gmail.com>
2023-03-31 13:34:32 -0500

    Se agrega la navegacion entre actividades de las pantallas existentes y vinculables

--- app/src/main/java/com/example/todo/Login_Facebook_P.kt
+++ app/src/main/java/com/example/todo/Login_Facebook_P.kt
@@ -1,11 +1,20 @@
 package com.example.todo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class Login_Facebook_P : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_facebook_p)
+
+        val retroceder = findViewById<ImageButton>(R.id.retroceder)
+
+        retroceder.setOnClickListener{
+            val intent:Intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/todo/Recover_Login_ToDo.kt
+++ app/src/main/java/com/example/todo/Recover_Login_ToDo.kt
@@ -1,11 +1,39 @@
 package com.example.todo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class Recover_Login_ToDo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recover_login_to_do)
+
+        val retroceder = findViewById<ImageButton>(R.id.retroceder)
+        val login1 = findViewById<Button>(R.id.login1)
+        val login_facebook = findViewById<Button>(R.id.login_facebook)
+        val login_google = findViewById<Button>(R.id.login_google)
+
+        retroceder.setOnClickListener{
+            val intent: Intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        login1.setOnClickListener{
+            val intent: Intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        login_facebook.setOnClickListener{
+            val intent: Intent = Intent(this, Login_Sign_Facebook::class.java)
+            startActivity(intent)
+        }
+
+        login_google.setOnClickListener{
+            val intent: Intent = Intent(this, Login_Sign_Google::class.java)
+            startActivity(intent)
+        }
     }
 }
