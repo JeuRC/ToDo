265fc586db5da094d65bdd10c03142a6cb701007
JeuRC <jeurojascastillo@gmail.com>
2023-03-31 23:53:53 -0500

    Se agrega el home

--- app/src/main/java/com/example/todo/Home.kt
+++ app/src/main/java/com/example/todo/Home.kt
@@ -1,11 +1,21 @@
 package com.example.todo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val imgTasks = findViewById<ImageView>(R.id.imgTasks)
+
+        imgTasks.setOnClickListener{
+            val intent: Intent = Intent(this, Tasks::class.java)
+            startActivity(intent)
+        }
     }
 }
