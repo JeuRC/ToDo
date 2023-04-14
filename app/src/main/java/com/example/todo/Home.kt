98749ab14852a22824755ac04f27838774e79f25
JeuRC <jeurojascastillo@gmail.com>
2023-04-14 15:42:42 -0500

    Se agregan las demas conexiones entre actividades

--- app/src/main/java/com/example/todo/Home.kt
+++ app/src/main/java/com/example/todo/Home.kt
@@ -3,18 +3,60 @@ package com.example.todo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.TextView
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        val btnProfile = findViewById<Button>(R.id.btnProfile)
         val imgTasks = findViewById<ImageView>(R.id.imgTasks)
+        val txtTasks = findViewById<TextView>(R.id.txtTasks)
+        val imgLists = findViewById<ImageView>(R.id.imgLists)
+        val txtLists = findViewById<TextView>(R.id.txtLists)
+        val imgCalendar = findViewById<ImageView>(R.id.imgCalendar)
+        val txtCalendar = findViewById<TextView>(R.id.txtCalendar)
+        val imgTeams = findViewById<ImageView>(R.id.imgTeams)
+        val txtTeams = findViewById<TextView>(R.id.txtTeams)
 
+        btnProfile.setOnClickListener{
+            val intent: Intent = Intent(this, Profile::class.java)
+            startActivity(intent)
+        }
         imgTasks.setOnClickListener{
             val intent: Intent = Intent(this, Tasks::class.java)
+            startActivity(intent)
+        }
+        txtTasks.setOnClickListener{
+            val intent: Intent = Intent(this, Tasks::class.java)
+            startActivity(intent)
+        }
+        imgLists.setOnClickListener{
+            val intent: Intent = Intent(this, Lists::class.java)
+            startActivity(intent)
+        }
+        txtLists.setOnClickListener{
+            val intent: Intent = Intent(this, Lists::class.java)
+            startActivity(intent)
+        }
+        imgCalendar.setOnClickListener{
+            val intent: Intent = Intent(this, Calendar::class.java)
+            startActivity(intent)
+        }
+        txtCalendar.setOnClickListener{
+            val intent: Intent = Intent(this, Calendar::class.java)
+            startActivity(intent)
+        }
+        imgTeams.setOnClickListener{
+            val intent: Intent = Intent(this, Teams::class.java)
+            startActivity(intent)
+        }
+        txtTeams.setOnClickListener{
+            val intent: Intent = Intent(this, Teams::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/todo/Profile.kt
+++ app/src/main/java/com/example/todo/Profile.kt
@@ -1,11 +1,26 @@
 package com.example.todo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class Profile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        val imgBack = findViewById<ImageButton>(R.id.imgBack)
+        val btnLagout = findViewById<Button>(R.id.btnLagout)
+
+        imgBack.setOnClickListener{
+            val intent: Intent = Intent(this, Home::class.java)
+            startActivity(intent)
+        }
+        btnLagout.setOnClickListener{
+            val intent: Intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
