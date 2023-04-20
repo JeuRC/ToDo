44a847f3374eb0460dfdd986fd2dc7e8bc2f7aae
JINNETH FERNANDA BETANCUR RAMIREZ <jbteancur64@uan.edu.co>
2023-04-19 23:28:30 -0500

    Se agrega funcion de guardar usuario y contraseña

--- app/src/main/java/com/example/todo/MainActivity.kt
+++ app/src/main/java/com/example/todo/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.example.todo
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    var username_principal: EditText?=null
+    var contraseña_principal: EditText?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        username_principal=findViewById(R.id.username_principal)
+        contraseña_principal=findViewById(R.id.contraseña_principal)
 
         val login1 = findViewById<Button>(R.id.login1)
         val signIn = findViewById<TextView>(R.id.singIn)
@@ -19,8 +30,17 @@ class MainActivity : AppCompatActivity() {
         val login_google = findViewById<Button>(R.id.login_google)
 
         login1.setOnClickListener{
-            val intent:Intent = Intent(this, Home::class.java)
-            startActivity(intent)
+            val prefuser=getSharedPreferences(username_principal?.text.toString(), Context.MODE_PRIVATE)
+            val password=prefuser.getString("Password", "")
+            if(password==contraseña_principal?.text.toString()){
+                val intent:Intent = Intent(this, Home::class.java)
+                startActivity(intent)
+                Toast.makeText(this,"Ha ingresado exitosamente", Toast.LENGTH_LONG).show()
+            } else{
+                Toast.makeText(this,"Usuario no registrado", Toast.LENGTH_LONG).show()
+            }
+            username_principal?.setText("")
+            contraseña_principal?.setText("")
         }
         signIn.setOnClickListener{
             val intent:Intent = Intent(this, Recover_Login_ToDo::class.java)
