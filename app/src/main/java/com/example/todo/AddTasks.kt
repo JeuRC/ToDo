70a7af1bcaaa6d3102b01b69ca172c8c13cfc752
CarytoCortes <dsandoval66@uan.edu.co>
2023-05-06 19:05:36 -0500

    Cambios en las actividades lists y Teams

--- app/src/main/java/com/example/todo/AddTasks.kt
+++ app/src/main/java/com/example/todo/AddTasks.kt
@@ -15,11 +15,15 @@ class AddTasks : AppCompatActivity() {
         setContentView(R.layout.activity_add_tasks)
 
         val btnAddTasks = findViewById<Button>(R.id.btnAddTasks)
-
+        val btnCancel = findViewById<Button>(R.id.btnCancel)
         btnAddTasks.setOnClickListener {
             val txtTitle = findViewById<EditText>(R.id.txtTitle)
             val intent = Intent(this, Tasks::class.java)
             intent.putExtra("task", txtTitle.text.toString())
+            startActivity(intent)
+        }
+        btnCancel.setOnClickListener{
+            val intent = Intent(this, Tasks::class.java)
             startActivity(intent)
         }
     }
